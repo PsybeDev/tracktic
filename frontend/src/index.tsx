370d4e920f734776df94dc7ea00ceafac3eaff65
JB Wood <jb@jbwood.io>
2024-03-26 22:18:26 -0400

    init live timing; upgrade wails

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,19 +2,16 @@
 import { render } from 'solid-js/web';
 import { Router, Route } from '@solidjs/router';
 import Settings from './Settings';
+import LiveTiming from './views/LiveTiming'
 import "./tailwind.css";
 
 import App from './App';
 
-const Home = () => (
-  <h1>Home</h1>
-)
-
 render(
   () => (
     <Router root={App}>
-      <Route path="/" component={Home} />
+      <Route path="/" component={LiveTiming} />
       <Route path="/settings" component={Settings} />
     </Router>
-    ),
+  ),
   document.getElementById('root') as HTMLElement);
---
+++ frontend/src/views/LiveTiming.tsx
@@ -0,0 +1,9 @@
+import type { Component } from "solid-js"
+import { createSignal } from "solid-js"
+
+const LiveTiming: Component = () => {
+
+  return (<div>Live timing </div>)
+}
+
+export default LiveTiming
