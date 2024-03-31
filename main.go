d3211b56cdfa0da7484e34cdd49ccb263dc955a9
JB Wood <jb@jbwood.io>
2024-03-31 08:20:40 -0400

    rename app to tracktic

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:             "razza",
+		Title:             "tracktic",
 		Width:             1024,
 		Height:            768,
 		MinWidth:          1024,
@@ -34,15 +34,15 @@ func main() {
 		StartHidden:       false,
 		HideWindowOnClose: false,
 		// BackgroundColour:  &options.RGBA{R: 255, G: 255, B: 255, A: 255},
-		Assets:            assets,
-		Menu:              nil,
-		Logger:            nil,
-		LogLevel:          logger.DEBUG,
-		OnStartup:         app.startup,
-		OnDomReady:        app.domReady,
-		OnBeforeClose:     app.beforeClose,
-		OnShutdown:        app.shutdown,
-		WindowStartState:  options.Normal,
+		Assets:           assets,
+		Menu:             nil,
+		Logger:           nil,
+		LogLevel:         logger.DEBUG,
+		OnStartup:        app.startup,
+		OnDomReady:       app.domReady,
+		OnBeforeClose:    app.beforeClose,
+		OnShutdown:       app.shutdown,
+		WindowStartState: options.Normal,
 		Bind: []interface{}{
 			app,
 		},
@@ -68,7 +68,7 @@ func main() {
 			WebviewIsTransparent: true,
 			WindowIsTranslucent:  true,
 			About: &mac.AboutInfo{
-				Title:   "razza",
+				Title:   "tracktic",
 				Message: "",
 				Icon:    icon,
 			},
