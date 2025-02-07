3f7b04f1a6d4598f0fbfa7abc10401d8c9175350
JB Wood <jb@jbwood.io>
2025-02-06 20:53:53 -0500

    remove wails code

--- frontend/src/views/LiveTiming.tsx
+++
@@ -1,9 +0,0 @@
-import type { Component } from "solid-js"
-import { createSignal } from "solid-js"
-
-const LiveTiming: Component = () => {
-
-  return (<div>Live timing </div>)
-}
-
-export default LiveTiming
